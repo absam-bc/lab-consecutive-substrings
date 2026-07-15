cc08322546fbd6fd11b95d2964a2d7ee9fd65032
Abdulsalam Ahmed <abdisalanahmed95@gmail.com>
2026-07-15 21:51:31 +0300

    done

--- consecutive_substrings.js
+++ consecutive_substrings.js
@@ -1,5 +1,14 @@
 function consecutiveSubstrings(string) {
   // type your code here
+   const result = [];
+
+  for (let i = 0; i < string.length; i++) {
+    for (let j = i + 1; j <= string.length; j++) {
+      result.push(string.slice(i, j));
+    }
+  }
+
+  return result;
 }
 
 if (require.main === module) {
@@ -11,6 +20,10 @@ if (require.main === module) {
 
   console.log("Expecting: ['a']");
   console.log("=>", consecutiveSubstrings('a'));
+  console.log("");
+
+  console.log("Expecting: []");
+  console.log("=>", consecutiveSubstrings(""));
 }
 
 module.exports = consecutiveSubstrings;
